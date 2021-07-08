012d0f58948cd8e7f092690036ea8bcb46f476f0
MingfengLi0122 <lmf950122@gmail.com>
2021-07-07 22:18:14 -0700

    Use axios to recieve api response and set the results

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,12 +1,22 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+import axios from "axios";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  
+  useEffect(() => {
 
+    const testUrl = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(testUrl)
+      .then(response => {
+        console.log("response -------->", response.data.results);
+        setResults([...response.data.results])
+      })
+  }, [term])
   return (
     <Fragment>
       <header className="logo">
